75dcd0d00d00a578e9b9dffbc155c7a43c1bcc16
SkyAo <csvwolf@qq.com>
2015-02-27 13:12:57 +0800

    base style finished

--- microblog.php
+++ microblog.php
@@ -21,32 +21,45 @@
 		<div class="contents">
 			<div class="blog-text">
 				<div class="text-main">
-					<textarea>123</textarea>
+					今天天气不错。
 				</div>
 				<div class="text-messages">
 					<ul>
-						<li>Time:</li>
-						<li>Edit</li>
-						<li>Delete</li>
+						<li id="time">Time:2015-2-27 12:06:09</li>
+						<div class="operate">
+							<li><a id="edit" href="#">Edit</a></li>
+							<li><a id="delete" href="#">Delete</a></li>
+						</div>
 					</ul>
 					<button id="cancel-edit">取消编辑</button>
 					<button id="confirm-edit">提交编辑</button>
 				</div>
 			</div>
-			<div class="blog-text">123</div>
-			<div class="blog-text">123</div>
-			<div class="blog-text">123</div>
-			<div class="blog-text">123</div>
-			<div class="blog-text">123</div>
+			<div class="blog-text">
+				<div class="text-main">
+					<textarea style="display:block;">今天天气不错。</textarea>
+				</div>
+				<div class="text-messages">
+					<ul style="display:none;">
+						<li id="time">Time:2015-2-27 12:06:09</li>
+						<div class="operate">
+							<li><a id="edit" href="#">Edit</a></li>
+							<li><a id="delete" href="#">Delete</a></li>
+						</div>
+					</ul>
+					<button style="display:block;" id="cancel-edit">取消编辑</button>
+					<button style="display:block;" id="confirm-edit">提交编辑</button>
+				</div>
+			</div>
 		</div>
 		<div class="pages">
 			<ul>
-				<li>Prev</li>
-				<li>1</li>
-				<li>2</li>
-				<li>3</li>
-				<li>4</li>
-				<li>Next</li>
+				<li><a href="#">Prev</a></li>
+				<li><a href="#">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+				<li><a href="#">Next</a></li>
 			</ul>
 		</div>
 	</div>
