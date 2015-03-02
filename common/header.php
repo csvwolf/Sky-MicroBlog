67c0f4e8db943044e412aef9faa66ea0bca50f71
SkyAo <csvwolf@qq.com>
2015-03-02 20:52:20 +0800

    delete extra files

--- common/header.php
+++ common/header.php
@@ -6,7 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, target-densitydpi=device-dpi">
 	<meta name="description" content="Just a MicroBlog">
 	<meta name="keywords" content="MicroBlog">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/core.js"></script>
